757d690f85be4db850b01357506118df12d44cc7
bemilio <34814729+bemilio@users.noreply.github.com>
2024-12-06 14:00:16 +0100

    Reverted to submission version

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 ## Receding-horizon solution of dynamic games
 
-This repository contains the code associated with the article [Linear-Quadratic Dynamic Games as Receding-Horizon Variational
-Inequalities](https://arxiv.org/submit/5818661). The code contains utilities for the computation of Open-Loop Nash equilibria and Closed-Loop Nash equilibria, both in the infinite-horizon unconstrained case and the finite-horizon, constrained case. 
+This repository contains the code associated with the article:
 
-Dependencies:
+**Linear-Quadratic Dynamic Games as Receding-Horizon Variational
+Inequalities** 
+*Emilio Benenati, Sergio Grammatico* 
+[arXiv preprint](https://arxiv.org/submit/5818661)
+DOI: [arXiv:2408.15703](https://doi.org/10.48550/arXiv.2408.15703)  
+
+The code contains utilities for the computation of Open-Loop Nash equilibria and Closed-Loop Nash equilibria, both in the infinite-horizon unconstrained case and the finite-horizon, constrained case. 
+
+###  Dependencies
 - [MPT3 toolbox](https://www.mpt3.org/), for the computation of forward-invariant polyhedra. Tested on v. 3.2.1
 - [bemilio/gfne:barebone](https://github.com/bemilio/gfne/tree/barebone), for the computation of finite-horizon, constrained Closed-Loop Nash equilibria. The code is a barebone fork of [forrestlaine/gfne](github.com/forrestlaine/gfne), see also [Laine et al, 2023]. Add the cloned repository to the MATLAB path.
 
-Minimal example script:
+### Running the code
 
 ```
 matlab/examples/basic_game
@@ -24,7 +31,20 @@ matlab/examples/4_zones_power_systems/main_4_zones_power_system.m
 matlab/examples/vehicle_platooning/main_vehicle_platooning.m
 ```
 
-The `python` code is currently under development and it should be considered as untested.
+### Publication data
 
+The simulation data used for the paper is available with DOI:
+```
+10.4121/ea21437d-6fb7-4b37-b640-e7cb53a56a45
+```
+
+To recreate the plots, load the `.mat` files on MATLAB and run, respectively:
+```
+matlab/examples/4_zones_power_systems/plot_4_zones_power_system.m
+```
+
+```
+matlab/examples/4_zones_power_systems/plot_vehicle_platooning.m
+```
 
 [Laine et a, 2023] Laine, Forrest, et al. *"The computation of approximate generalized feedback nash equilibria."* SIAM Journal on Optimization (2023): 294-318.
--- matlab/examples/vehicle_platooning/main_vehicle_platooning.m
+++ matlab/examples/vehicle_platooning/main_vehicle_platooning.m
@@ -10,8 +10,7 @@ rmpath(genpath('../')) % remove folders of the other examples (function names ar
 addpath(genpath(pwd)) % re-add the subfolders of this example
 
 
-run_cl = true;
-use_cl_surrogate = true;
+run_cl = false;
 
 seed = 1;
 rng(seed); 
@@ -67,7 +66,8 @@ err_shift = zeros(N_tests,1);
 %% Create an initial state in position-velocity coordinates and convert 
 % % Position is relative with the first agent and meas. unit is meters
 x_0_p = (0:-90:-90*(N-1))' +  [0;15*randn(N-1,1)];
-x_0_v = (param.max_speed - param.min_speed)/2 + .3* param.min_speed + (.5-rand(N,1)) .* min(param.max_speed - param.min_speed);
+x_0_v = (param.max_speed - param.min_speed)/2 + param.min_speed...
+                         + (.5-rand(N,1)) .* min(param.max_speed - param.min_speed);
 
 x_0 = convertPosVelToState(x_0_p, x_0_v, param.v_des_1, param.d_des, param.headway_time);
 
@@ -82,34 +82,26 @@ while test<N_tests + 1
     x_bl(:,:,1, test) = x_0(:,:,test);
         
     if isInfHorStable_ol
-        game.VI_gen_OL = computeVIGenerator(game, T);
-        [~, ~, A_sh, ~, ~, ~] = game.VI_gen_OL(x_0(:,:,test)); % Computed here just to get the number of shared constrains and initialize the dual
+        game.VI_generator = computeVIGenerator(game, T);
+        [~, ~, A_sh, ~, ~, ~] = game.VI_generator(x_0(:,:,test)); % Computed here just to get the number of shared constrains and initialize the dual
         n_sh_constraints = size(A_sh,1);
-        dual_ol = zeros(n_sh_constraints, 1);
-        if use_cl_surrogate
-            game.VI_gen_surrogate_CL = computeVIGeneratorSurrogateCL(game,T);
-            [~, ~, A_sh_cl, ~, ~, ~] = game.VI_gen_surrogate_CL(x_0(:,:,test)); % Computed here just to get the number of shared constrains and initialize the dual
-            n_sh_constraints_cl = size(A_sh_cl,1);
-            dual_cl = zeros(n_sh_constraints_cl,1);
-        end
+        dual = zeros(n_sh_constraints, 1);
     end
     t_OL_assumpt_satisfied = zeros(N_tests,1);
     for t=1:T_sim
         disp("timestep = " + num2str(t));
         if t>1
             u_ol_warm_start = u_shift_ol(:,:, :, t-1);
-            u_cl_warm_start = u_shift_cl(:,:, :, t-1);
         else
             u_ol_warm_start = zeros(n_u * T, 1, N);
-            u_cl_warm_start = zeros(n_u * T, 1, N);
         end
 
         %% Solve open-loop MPC problem
         if isInfHorStable_ol
             [VI.J, VI.F, VI.A_sh, VI.b_sh, VI.A_loc, VI.b_loc, VI.n_x, VI.N]...
-                = game.VI_gen_OL(x_ol(:,:,t,test));
-            dual_warm_start = dual_ol;
-            [u_full_traj_ol(:,:,:,t), dual_ol, res, solved(t)] = solveVICentrFB(VI, 10^6, eps, ...
+                = game.VI_generator(x_ol(:,:,t,test));
+            dual_warm_start = dual;
+            [u_full_traj_ol(:,:,:,t), dual, res, solved(t)] = solveVICentrFB(VI, 10^6, eps, ...
                 0.2, 0.2, u_ol_warm_start, dual_warm_start);
             u_full_traj_ol(:,:,:,t) = u_full_traj_ol(:,:,:,t);
             u_ol(:,:,:,t,test) = u_full_traj_ol(1:n_u,:,:,t);
@@ -134,15 +126,8 @@ while test<N_tests + 1
         %% Solve closed-loop MPC problem 
         if isInfHorStable_cl && run_cl
             
-            if use_cl_surrogate
-                [VI.J, VI.F, VI.A_sh, VI.b_sh, VI.A_loc, VI.b_loc, VI.n_x, VI.N]...
-                                = game.VI_gen_surrogate_CL(x_cl(:,:,t,test));
-                dual_warm_start = dual_cl;
-                [u_full_traj_cl(:,:,:,t), dual_cl, res, solved(t)] = solveVICentrFB(VI, 10^6, eps, ...
-                    0.2, 0.2, u_cl_warm_start, dual_warm_start);
-            else
-                [~, ~, u_full_traj_cl(:,:,:,t)] = solveImplFinHorCL(game, T, x_cl(:,:,t,test));
-            end
+            [~, ~, u_full_traj_cl(:,:,:,t)] = solveImplFinHorCL(game, T, x_cl(:,:,t,test));
+            u_full_traj_cl(:,:,:,t) = u_full_traj_cl(:,:,:,t);
             u_cl(:,:,:,t,test) = u_full_traj_cl(1:n_u,:,:,t);
             x_cl(:,:,t+1,test) = evolveState(x_cl(:,:,t,test), game.A, game.B, u_cl(:, :,:, t,test), 1, n_u);
             % Retrieve last state, used for computing the shifted trajectory
@@ -161,12 +146,14 @@ while test<N_tests + 1
         end
     end 
     err_shift(test) = 0;
-    for t=t_OL_assumpt_satisfied(test):T_sim-1
-        for i=1:N
-            err_shift(test) = max(err_shift(test), norm(u_shift_ol(:,:,i,t) - u_full_traj_ol(:,:,i, t+1)));
+    % if is_test_valid_ol(test)
+        for t=t_OL_assumpt_satisfied(test):T_sim-1
+            for i=1:N
+                err_shift(test) = max(err_shift(test), norm(u_shift_ol(:,:,i,t) - u_full_traj_ol(:,:,i, t+1)));
+            end
         end
-    end
-    disp("err test = " + num2str(err_shift(test)))
+        disp("err test = " + num2str(err_shift(test)))
+    % end
     test = test+1;
 end
 
