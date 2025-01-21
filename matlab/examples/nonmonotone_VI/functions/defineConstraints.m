34cef1adb56f6193342680a09ea91ee94c78f367
bemilio <34814729+bemilio@users.noreply.github.com>
2025-01-21 10:59:39 +0100

    Removed broken obj. computation. Added nonmonotonicity example

--- matlab/examples/nonmonotone_VI/functions/defineConstraints.m
+++ matlab/examples/nonmonotone_VI/functions/defineConstraints.m
@@ -1,9 +1,9 @@
 function [C_x, d_x, C_u, d_u, C_x_mix, C_u_mix, d_mix] = defineConstraints(N, n_x, n_u)
 
-    min_u = -ones(n_u, 1, N); 
-    max_u = ones(n_u, 1, N);
-    min_u(:,:,2)=0.1*min_u(:,:,2);
-    max_u(:,:,2)=0.1*max_u(:,:,2);
+    min_u = -100*ones(n_u, 1, N); 
+    max_u = 100*ones(n_u, 1, N);
+    min_u(:,:,1)=1*min_u(:,:,1); 
+    max_u(:,:,1)=1*max_u(:,:,1);
     
     d_x = [1]; % dummy
     C_x = zeros(1,n_x); % dummy
@@ -13,9 +13,9 @@ function [C_x, d_x, C_u, d_u, C_x_mix, C_u_mix, d_mix] = defineConstraints(N, n_
         C_u(:,:,i) = [-eye(n_u); eye(n_u)];
         d_u(:,:,i) = [-min_u(:,:,i); max_u(:,:,i)]; 
     end
+
     C_x_mix = zeros(1,n_x); % dummy
     C_u_mix = zeros(1, n_u, N); % dummy
     d_mix = 1; % dummy
-
 end
 
