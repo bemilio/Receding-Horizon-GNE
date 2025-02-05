fb1fb4a9cba9af0194ab51818a9a29b78ba55e82
Emilio Benenati <34814729+bemilio@users.noreply.github.com>
2025-02-05 13:08:35 +0100

    Revert "Plot distance from term set"

--- matlab/dyn_games/checkTerminalConditionOL.m
+++ matlab/dyn_games/checkTerminalConditionOL.m
@@ -1,6 +1,10 @@
 function [flag] = checkTerminalConditionOL(x, X_f)
 
-flag = X_f.contains(x);
+flag = true;
+for i=1:length(X_f)
+    x_lifted = [x;x];
+    flag = flag & X_f{i}.contains(x_lifted);
+end
 
 end
 
